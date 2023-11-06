f0499eb8dff085e29c76683382f146c4ebf1393a
Zakerous76 <m.z.humayoon@gmail.com>
2023-11-06 17:27:24 +0300

    step-2

--- myfirsttutorial/lib/main.dart
+++ myfirsttutorial/lib/main.dart
@@ -2,8 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myfirsttutorial/firebase_options.dart';
+import 'package:myfirsttutorial/views/login_view.dart';
+import 'package:myfirsttutorial/views/register_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure that Flutter is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     // we are straight up returning the Material from here rather than returning a separate MyApp instance becasue this yields more performance
     title: 'Flutter Demo',
@@ -12,6 +20,11 @@ void main() {
       useMaterial3: true,
     ),
     home: const HomePage(),
+    routes: {
+      "/login/": (context) =>
+          const LoginView(), // returns an instance of the loginview
+      "/register/": (context) => const RegisterView(),
+    },
   ));
 }
 
@@ -20,28 +33,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:
-          AppBar(title: const Text("Home Page"), backgroundColor: Colors.blue),
-      body: FutureBuilder(
-          future: Firebase.initializeApp(
-              // if its not initialized, the instance of firebase may be null
-              options: DefaultFirebaseOptions.currentPlatform),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                final user = FirebaseAuth.instance.currentUser;
-                // "If the user is non-null, take it. If the user is null then take false"
-                if (user?.emailVerified ?? false) {
-                  print("You are verified my nigga!");
-                } else {
-                  print("You are NOT verified my nigga!");
-                }
-                return const Text("Done");
-              default:
-                return const Text("Loading...");
-            }
-          }),
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+            // if its not initialized, the instance of firebase may be null
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              // final user = FirebaseAuth.instance.currentUser;
+              // // "If the user is non-null, take it. If the user is null then take false"
+              // if (user?.emailVerified ?? false) {
+              //   return const Text("Done");
+              // } else {
+              //   print("You are NOT verified, you need to verify my nigga!");
+              //   // Navigator.of(context).push(MaterialPageRoute(          // Since we are returning a "Column" rather than a scaffold widget from the VerifyEmailView()
+              //   //     builder: (context) => const VerifyEmailView()));
+              // }
+              return const LoginView();
+
+            default:
+              return const CircularProgressIndicator();
+          }
+        });
   }
 }
--- myfirsttutorial/lib/views/register_view.dart
+++ myfirsttutorial/lib/views/register_view.dart
@@ -36,79 +36,93 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     // To make sure that the core flutter engine is in place.
     WidgetsFlutterBinding.ensureInitialized();
-
-    // Scaffold of the entire widget area, where the user can interact and etc.
+    // We are not going to return a scaffold widget but rather a column widget which is going to replace the "body" of the HomePage's scaffold.
+    //  Well, technically, this page will return a column widget to the future builder of the HomePage scaffold.
     return Scaffold(
-      appBar: AppBar(title: const Text("Register")),
-
-      // The body of the scaffold is wrapped inside a FutureBuilder widget because we want to build the widget based on the information we get from the future
-      // Creates a widget that builds itself based on the latest snapshot of interaction with a [Future].
-      body: FutureBuilder(
-        // The future that it awaits is this:
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        // Then, it will build this, using the snapshot of the  future above:
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: "Enter your email here",
-                    ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: "Enter your password here",
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      print("Button Pressed");
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print("User logged in: ${userCredential.user?.email}");
-                      } catch (e) {
-                        if (e is FirebaseAuthException) {
-                          if (e.code == "weak-password")
-                            print("Weak Password");
-                          else if (e.code == "email-already-in-use")
-                            print("Email already in use my nigga!");
-                          else if (e.code == "invalid-email")
-                            print("Invalid Email my nigga!");
-                          else {
-                            print('Firebase Authentication Error: ${e.code}');
-                            print(
-                                'Firebase Authentication Error Message: ${e.message}');
-                          }
-                        } else {
-                          print('Error: $e');
-                        }
-                      }
-                    },
-                    child: const Text("Register"),
-                  ),
-                ],
-              );
-            default:
-              return const Text("Loading...");
-          }
-        },
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: "Enter your email here",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: "Enter your password here",
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              print("Button Pressed");
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print("User logged in: ${userCredential.user?.email}");
+              } catch (e) {
+                if (e is FirebaseAuthException) {
+                  if (e.code == "weak-password")
+                    print("Weak Password");
+                  else if (e.code == "email-already-in-use")
+                    print("Email already in use my nigga!");
+                  else if (e.code == "invalid-email")
+                    print("Invalid Email my nigga!");
+                  else {
+                    print('Firebase Authentication Error: ${e.code}');
+                    print(
+                        'Firebase Authentication Error Message: ${e.message}');
+                  }
+                } else {
+                  print('Error: $e');
+                }
+              }
+            },
+            child: const Text("Register"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil("/login/", (route) => false);
+            },
+            child: const Text("Already registered? Login here!"),
+          )
+        ],
       ),
     );
+    // Scaffold of the entire widget area, where the user can interact and etc.
+    // return Scaffold(
+    //   appBar: AppBar(title: const Text("Register")),
+
+    //   // The body of the scaffold is wrapped inside a FutureBuilder widget because we want to build the widget based on the information we get from the future
+    //   // Creates a widget that builds itself based on the latest snapshot of interaction with a [Future].
+    //   body: FutureBuilder(
+    //     // The future that it awaits is this:
+    //     future: Firebase.initializeApp(
+    //       options: DefaultFirebaseOptions.currentPlatform,
+    //     ),
+    //     // Then, it will build this, using the snapshot of the  future above:
+    //     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+    //       switch (snapshot.connectionState) {
+    //         case ConnectionState.done:
+    //           return (******************)
+    //         default:
+    //           return const Text("Loading...");
+    //       }
+    //     },
+    //   ),
+    // );
   }
 }
