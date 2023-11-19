6eb4334fb5988b5cacbefb5b9f70ebec7e849d1e
Zakerous76 <m.z.humayoon@gmail.com>
2023-11-19 12:54:05 +0300

    Step 12: Created a Singleton of the NotesService/DB. Made sure that the DB is open before any operation. Added Email to AuthUser and made necessary mods in other documents. Added a FutureBuilder and in it a StreamBuilder in notes.view to display notes from _notesStreamController.

--- myfirsttutorial/lib/services/auth/auth_user.dart
+++ myfirsttutorial/lib/services/auth/auth_user.dart
@@ -3,10 +3,19 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
+  // since notes from the database are retrieved using emails, we need AuthUser to have an email because UI accesses a Firebase using AuthService and consequently, AuthUser
+  // String is optional (String?) because the email getter for User in its definition is also optional String.
+  // But in our application, there always IS an email because in order to use the application, one must have an email.
+  final String? email;
   final bool isEmailVerified;
-  const AuthUser({required this.isEmailVerified});
+  const AuthUser({
+    required this.email,
+    required this.isEmailVerified,
+  });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
+        // The AuthUser's email is read from the Firebase User
+        email: user.email,
         isEmailVerified: user.emailVerified,
       );
 }
